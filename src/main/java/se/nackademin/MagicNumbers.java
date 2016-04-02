31f88407ed1f9628628077421f96473aa8bee7e3
Peter Björnberg <peter.bjornberg@yh.nackademin.se>
2016-04-02 11:21:51 +0200

    Complete implementation MagicNumbers.java

--- src/main/java/se/nackademin/MagicNumbers.java
+++ src/main/java/se/nackademin/MagicNumbers.java
@@ -1,5 +1,7 @@
 package se.nackademin;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class MagicNumbers {
 
     private String name;
@@ -8,29 +10,68 @@ public class MagicNumbers {
     private int age;
     private int height;
 
+    /**
+     * @return integer value between 0 and 9 
+     */    
     public int calculateA() {
-        //TODO: calculate A
-        return 0;
+        
+        int a = 0;
+        a = StringUtils.countMatches(name, " ") + 1;
+        a += age;
+        for(;a>9;a-=7);
+        return a;
     }
-
+    
+    /**
+     * @return integer value between 0 and 9 
+     */
     public int calculateB() {
-        //TODO: calculate B
-        return 0;
-    }
 
+        int b = 0;
+        b = location.length();
+        b += income;
+        for(;b>9;b-=7);
+        return b;
+    }
+    
+    /**
+     * @return integer value between 0 and 9 
+     */
     public int calculateC() {
-        //TODO: calculate C
-        return 0;
+
+        int c = 0;
+        c = calculateA() + calculateB() - height;
+        for(;c<0;c+=10);
+        return c;
     }
 
+    
+    /**
+    * @return integer value between 0 and 9 
+    */
     public int calculateD() {
-        //TODO: calculate D
-        return 0;
+
+        int d = 0;
+        int a = calculateA();
+        d = (a > 5) ? a + calculateB() :  a + calculateC();
+        d -= income;
+        for(;d<0;d+=10);
+        return d;
     }
 
+    /**
+     * @return integer value between 0 and 9 
+     */
     public int calculateE() {
-        //TODO: calculate E
-        return 0;
+        long e = 0;
+        e = (long)age * (long)income * (long)income * (long)height;
+        double eFloat = Math.sqrt(e);
+        System.out.println(eFloat);
+
+        for(;eFloat>9;eFloat/=2);
+        
+        e = Math.round(eFloat);
+        return (int)e;
     }
 
     public void setName(String name) {
